01c735e2711e76ec670a8c868aa80566d05c9d9d
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2016-11-21 10:36:45 +0100

    template engine

--- 03-templates/src/app.js
+++ 03-templates/src/app.js
@@ -1,12 +1,25 @@
 import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
-import magnusTpl from './templates/magnus.hbs'
-import sergeyTpl from './templates/sergey.hbs'
+import playerTpl from './templates/player.hbs'
 import contactTpl from './templates/contact.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 const $app = $('#app')
+
+const all_Players = {
+  magnus: {
+    name: 'Magnus Carlsen',
+    image: "https://cdn.worldchess.com/static/img/nyfide/carlsen_2x.png",
+    description: 'Lorem ipsum'
+  },
+
+  sergey: {
+    name: 'Sergey Karjakin',
+    image: "https://cdn.worldchess.com/static/img/nyfide/karjakin_2x.png",
+    description: 'Lorem ipsum'
+  }
+}
 
 function index() {
   $app.html(homeTpl())
@@ -17,16 +30,15 @@ function contact() {
 }
 
 function players(ctx) {
-  let tpl = () => {}
-  switch (ctx.params.player) {
-    case 'magnus':
-      tpl = magnusTpl
-      break;
-    case 'sergey':
-      tpl = sergeyTpl
-      break;
-  }
-  $app.html(tpl())
+  const player = all_Players[ctx.params.player];
+  if(player==undefined)
+    notFound();
+  else{
+  var name = player.name;
+  var image = player.image;
+  var description = player.description;
+  $app.html(playerTpl({name, image, description}));
+}
 }
 
 function notFound() {
